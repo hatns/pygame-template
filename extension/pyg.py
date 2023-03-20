d19a8872fff808e71630f1633cd29bca9cd763a9
hatns <127141603+hatns@users.noreply.github.com>
2023-03-20 21:41:15 +0100

    added inputhandler

--- extension/pyg.py
+++ extension/pyg.py
@@ -1,6 +1,6 @@
-from pygame import *
 import pygame
 
+# Should game be running? Decided by this class
 class GameRunner:
     def __init__(self):
         self.is_running = True
@@ -8,6 +8,7 @@ class GameRunner:
         self.is_running = False
         pygame.quit()
 
+# All functions to run before updates
 class FunctionQueue:
     def __init__(self):
         self.functions = []
@@ -27,11 +28,25 @@ class EventHandler:
             return
         self.event_mapper[event] = ([trigger_function, function_args])
     def update(self):
+        global event
         for event in pygame.event.get():
             if event.type in self.event_mapper:
                 for i in range(0, len(self.event_mapper[event.type]), 2):
                     self.event_mapper[event.type][i](*self.event_mapper[event.type][i+1])
 
+class InputHandler():
+    def __init__(self):
+        self.keys_pressed = []
+    
+    def key_pressed(self):
+        global event
+        self.keys_pressed.append(pygame.key.name(event.key))
+
+    def key_released(self):
+        global event
+        self.keys_pressed.remove(pygame.key.name(event.key))
+
+# Gamehandler structure
 class GameHandler:
     def __init__(self, updates_per_second, quicksetup: bool = True, eventhandler: EventHandler = EventHandler(), 
                 clock: pygame.time.Clock = pygame.time.Clock, gamerunner: GameRunner = GameRunner()):
@@ -42,8 +57,11 @@ class GameHandler:
         self.clock = clock
         self.fps = updates_per_second
         self.functionqueue = FunctionQueue()
+        self.inputhandler = InputHandler()
         if quicksetup:
-            self.eventhandler.add(QUIT, self.game.quit)
+            self.eventhandler.add(pygame.KEYDOWN, self.inputhandler.key_pressed)
+            self.eventhandler.add(pygame.KEYUP, self.inputhandler.key_released)
+            self.eventhandler.add(pygame.QUIT, self.game.quit)
 
     def add_function(self, function, *parameters):
         self.functionqueue.add(function, parameters)
--- template.py
+++ template.py
@@ -1,15 +1,16 @@
 # Imports
-import extension.pyg as pygame
+import extension.pyg as pyg
+import pygame
 
 # Global variables
-screen_dimensions = (1080, 720)
+screen_dimensions = (700, 500)
 frames_per_second = 60
 
 # Initiate PyGame
 window = pygame.display.set_mode(screen_dimensions)
 
 # Initiate GameHandler
-gamehandler = pygame.GameHandler(frames_per_second)
+gamehandler = pyg.GameHandler(frames_per_second)
 
 # Main Loop
 while gamehandler.game.is_running:
